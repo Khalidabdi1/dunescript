7c7bbd2a559bc188afa39b6d38b2db1b66a0690a
Khalid Abdi <xamza2009@gmail.com>
2025-08-03 00:42:26 +0300

    now the package to tell you last version of the package

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 #!/usr/bin/env node
 
-console.log("Welcome to DuneScript!");
+import axios from 'axios';
+
+async function main() {
+  const args = process.argv.slice(2);
+
+  if (args.length === 0) {
+    console.log("Please provide a command.");
+    process.exit(1);
+  }
+
+  const command = args[0];
+
+  switch(command) {
+    case "hello":
+      console.log("Hello from DuneScript!");
+      break;
+
+    case "check":
+      const pkg = args[1];
+      if (!pkg) {
+        console.log("Please specify a package to check.");
+      } else {
+        console.log(`Checking package: ${pkg} ...`);
+        try {
+          const res = await axios.get(`https://registry.npmjs.org/${pkg}`);
+          const latestVersion = res.data['dist-tags'].latest;
+          console.log(`Latest version: ${latestVersion}`);
+          // TODO: المزيد من التحليل هنا
+        } catch (err) {
+          console.error("Failed to fetch package info:", err.message);
+        }
+      }
+      break;
+
+    default:
+      console.log(`Unknown command: ${command}`);
+  }
+}
+
+main();
